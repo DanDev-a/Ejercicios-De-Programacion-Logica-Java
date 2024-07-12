c3ea6ea6d997d4d2b297937a3759f13bab50237e
Lobul24 <kalefinley@gmail.com>
2024-07-12 10:33:36 -0400

    Se añadio el primer ejercicio

---
+++ Ejercicios-Logica/Logica01.java
@@ -0,0 +1,23 @@
+public class Logica01 {
+
+    /*1) Declara dos variables numéricas (con el valor que desees), 
+    muestra por consola la suma, resta, multiplicación, división y 
+    módulo (resto de la división).*/
+    public static void main(String[] args) throws Exception {
+        
+        //Declaramos las dos variables que operamos
+        int num1 = 10;
+        int num2 = 5;
+
+        //Mostramos los Resultados de las operaciones
+        System.out.println("Resultado de la Suma: " + (10+5));
+
+        System.out.println("Resultado de la Resta: " + (10-5));
+
+        System.out.println("Resultado de la Multiplicacion: " + (10*5));
+
+        System.out.println("Resultado de la Division: " + (10/5));
+
+        System.out.println("Resultado de la Division (Modulo): " + (10%5));
+    }
+}
